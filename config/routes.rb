30d6ed5aa2bff25c653f732b179673634b799e26
Syoma Iwauchi <syoma4645@gmail.com>
2020-07-06 13:42:16 +0900

    routes-file create

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  
+  # トップページ
   root to: 'homes#home'
 
+  # 料金一覧 よくある質問 お問い合わせ
+  get 'homes/plan', to: 'homes#plan'
+  get 'homes/contact', to: 'homes#contact'
+  get 'homes/question', to: 'homes#question'
+
+  # 商品一覧
   resources :products do
   end
+  
 end
