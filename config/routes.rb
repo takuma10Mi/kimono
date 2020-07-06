1937b5126db1785589ea1bef229184b12bf274b6
Syoma Iwauchi <syoma4645@gmail.com>
2020-07-06 14:52:55 +0900

    buy-page cteate

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,17 @@ Rails.application.routes.draw do
 
   # 料金一覧 よくある質問 お問い合わせ
   get 'homes/plan', to: 'homes#plan'
-  get 'homes/contact', to: 'homes#contact'
   get 'homes/question', to: 'homes#question'
+  get 'homes/contact', to: 'homes#contact'
 
   # 商品一覧
   resources :products do
+    # idは不要なのでcollectionを使用
+    collection do
+      # 商品購入ページ 購入完了ページ
+      get 'checkout'
+      get 'done'
+     end
   end
   
 end
