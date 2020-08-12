dc1ad009a5175a1630f8e630f1be54c402c7fa76
Syoma Iwauchi <syoma4645@gmail.com>
2020-08-12 13:52:27 +0900

    responsed total100%

--- app/assets/javascripts/navi.js
+++ app/assets/javascripts/navi.js
@@ -10,4 +10,9 @@ window.onload = function(){
     $('.buttom').toggleClass('hidden');
     $('.buttom__btn').toggleClass('hidden');
   });
+
+  $(document).on('click', '.checkout__container__content__body__box__right__response', function(){
+    $('.checkout__container__content__body__box__right__response__text').toggleClass('hidden');
+    $('.checkout__container__content__body__box__right__overview').toggleClass('hidden');
+  });
 };
